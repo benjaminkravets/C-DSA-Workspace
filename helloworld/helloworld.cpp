f31324936ddde504d255464359cf4a3de9597bf8
Benjamin Kravets <bakravets@gmail.com>
2023-09-18 11:57:04 -0400

    add array offset sample

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,17 +1,20 @@
+//derived from geeksforgeeks.com
 #include <iostream>
 #include <vector>
- 
+
 using namespace std;
+
+const int l = 4;
 
 int main()
 {
-    int x[2][4] = {{0, 1, 2, 3}, {4, 5, 6, 7}};
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 4; j++){
-            cout << x[i][j] << endl;
-        }
-    }
+    int arr2[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    
+    cout << arr2[1][3] << endl;
+    cout << *(*(arr2+1)+3) << endl;
 
-  
-    return 0;
+    int arr3[2][3][2] = { {{1, 2}, {3, 4}, {5, 6}}, {{7, 8}, {9, 10}, {11, 12}} };
+
+    cout << arr3[1][1][0] << endl;
+    cout << *(*(*(arr3+1)+1))+0 << endl;
 }
