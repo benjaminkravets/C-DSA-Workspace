af9a0e63b4a9162e5a9f049819f270e0daa5b7f4
Benjamin Kravets <bakravets@gmail.com>
2023-09-19 16:58:13 -0400

    add string demos

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,22 +1,43 @@
 //derived from geeksforgeeks.com
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <string.h>
 
 using namespace std;
 
-void fun(char* arr)
-{
-    int i;
-    unsigned int n = strlen(arr);
-    cout << "n = " << n << endl;
-    for (i = 0; i < n; i++)
-        cout << arr[i] << " ";
-}
- 
-// Driver program
 int main()
 {
-    char arr[] = "geeksquiz";
-    fun(arr);
+    // Initializing string
+    string str = "geeksforgeeks is for geeks";
+  
+    // Displaying string
+    cout << "The initial string is : ";
+    cout << str << endl;
+  
+    // Resizing string using resize()
+    str.resize(13);
+  
+    // Displaying string
+    cout << "The string after resize operation is : ";
+    cout << str << endl;
+  
+    // Displaying capacity of string
+    cout << "The capacity of string is : ";
+    cout << str.capacity() << endl;
+  
+    // Displaying length of the string
+    cout << "The length of the string is :" << str.length()
+         << endl;
+  
+    // Decreasing the capacity of string
+    // using shrink_to_fit()
+    str.shrink_to_fit();
+  
+    // Displaying string
+    cout << "The new capacity after shrinking is : ";
+    cout << str.capacity() << endl;
+  
     return 0;
 }
