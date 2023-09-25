89b29972849575f4b2eefe0a839a9a47c3d7f2b0
Benjamin Kravets <bakravets@gmail.com>
2023-09-25 16:57:58 -0400

    add variant member union

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -7,16 +7,30 @@
 #include <regex>
 using namespace std;
 
-struct Cpp_Struct
-{
-  public:
-    const int i;
-    int k;
-    Cpp_Struct():i(2),k(3){}
+// base class
+union geek {
+    // Defining data members
+    int age;
+    char grade;
+    float GPA;
 };
+  
 int main()
 {
-    Cpp_Struct obj1;
-    cout << "Struct with constant members: " << obj1.i << " " << obj1.k << endl;
+  
+    // Defining a union variable
+    union geek student1;
+  
+    // Assigning values to data member of union geek and
+    // printing the values of data members
+    student1.age = 25;
+    cout << "Age : " << student1.age << endl;
+  
+    student1.grade = 'B';
+    cout << "Grade : " << student1.grade << endl;
+  
+    student1.GPA = 4.5;
+    cout << "GPA : " << student1.GPA << endl;
+  
     return 0;
 }
