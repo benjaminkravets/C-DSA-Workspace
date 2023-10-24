95faaac08a183547f3eb14420dcabc9216a703a0
Benjamin Kravets <bakravets@gmail.com>
2023-10-24 17:00:22 -0400

    non-object class function call via statics

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -2,30 +2,41 @@
 #include<string.h>
 using namespace std;
 
-
-class A {
-public:
-    A() 
-    { 
-      cout << "A's Constructor Called " << 
-               endl; 
+class Box  
+{  
+    private:  
+    static int length; 
+    static int breadth;  
+    static int height;  
+     
+    public:
+     
+    static void print()  
+    {  
+        cout << "The value of the length is: " << length << endl;  
+        cout << "The value of the breadth is: " << breadth << endl;  
+        cout << "The value of the height is: " << height << endl;  
     }
-};
+};  
  
-class B {
-    static A a;
+// initialize the static data members  
  
-public:
-    B() 
-    { 
-      cout << "B's Constructor Called " << 
-               endl; 
-    }
-};
+int Box :: length = 10;  
+int Box :: breadth = 20;  
+int Box :: height = 30;  
  
-// Driver code
-int main()
+// Driver Code
+   
+int main()  
 {
-    B b;
-    return 0;
+     
+    Box b;  
+     
+    cout << "Static member function is called through Object name: \n" << endl;  
+    b.print();  
+     
+    cout << "\nStatic member function is called through Class name: \n" << endl;  
+    Box::print();  
+     
+    return 0;  
 }
