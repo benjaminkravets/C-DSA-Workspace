a513041f8823e5df423c5aab8926b4caa9905ac7
Benjamin Kravets <bakravets@gmail.com>
2023-11-01 16:59:25 -0400

    is_number demo

--- dateutility.py
+++ dateutility.py
@@ -1,16 +1,19 @@
 import pyautogui as pag
 import ctypes
+import pyperclip
+import numbers
+import decimal
 
-print(pag.size())
+#curlay = pyperclip.paste()
+
+def is_number(curlay):
+    try:
+        tmp = float(curlay)
+        print('The variable a number')
+    except:
+        print('The variable is not a number')
 
 
-
-submit_button = None
-while submit_button is None:
-    submit_button = pag.locateOnScreen(image="target.png", confidence=.7)
-
-print(submit_button)
-
-
-pag.moveTo(submit_button, duration=1)
-#pag.click()
+is_number(' ')
+is_number(1)
+is_number(1.2)
