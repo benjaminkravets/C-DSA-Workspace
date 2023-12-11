a83f76b64ed67e635acd38e2f5980960a751ec26
Benjamin Kravets <bakravets@gmail.com>
2023-12-11 16:57:12 -0500

    inheritance lesson

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,35 +1,32 @@
 // Linked list implementation in C++
 
-//#include <bits/stdc++.h>
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
-class Complex {
-private:
-    int real, imag;
- 
-public:
-    Complex(int r = 0, int i = 0)
-    {
-        real = r;
-        imag = i;
-    }
- 
-    // This is automatically called when '+' is used with
-    // between two Complex objects
-    Complex operator+(Complex const& obj)
-    {
-        Complex res;
-        res.real = real + obj.real;
-        res.imag = imag + obj.imag;
-        return res;
-    }
-    void print() { cout << real << " + i" << imag << '\n'; }
-};
- 
-int main()
-{
-    Complex c1(10, 5), c2(2, 4);
-    Complex c3 = c1 + c2;
-    c3.print();
+// Base class 
+class Parent { 
+public: 
+    int id_p; 
+}; 
+  
+// Sub class inheriting from Base Class(Parent) 
+class Child : public Parent { 
+public: 
+    int id_c; 
+}; 
+  
+// main function 
+int main() 
+{ 
+    Child obj1; 
+  
+    // An object of class child has all data members 
+    // and member functions of class parent 
+    obj1.id_c = 7; 
+    obj1.id_p = 91; 
+    cout << "Child id is: " << obj1.id_c << '\n'; 
+    cout << "Parent id is: " << obj1.id_p << '\n'; 
+  
+    return 0; 
 }
