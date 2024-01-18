f60c15feec6c48d5ed084b9be588e8e83cad37be
Benjamin Kravets <bakravets@gmail.com>
2024-01-18 11:28:14 -0500

    iterators and algorithms lesson

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main()
+{
+    // Initializing vector with array values
+    int arr[] = {5, 10, 15, 20, 20, 23, 42, 45};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> vect(arr, arr+n);
+
+    cout << distance(vect.begin(), max_element(vect.begin(), vect.end()));
+ 
+    return 0;
+}
