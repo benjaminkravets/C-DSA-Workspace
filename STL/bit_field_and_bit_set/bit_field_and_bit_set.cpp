31cc2d95f6913425553238e48f6816f18330cf8c
Benjamin Kravets <bakravets@gmail.com>
2024-02-10 20:37:26 -0500

    add bitfield manipulation

--- STL/bit_field_and_bit_set/bit_field_and_bit_set.cpp
+++ STL/bit_field_and_bit_set/bit_field_and_bit_set.cpp
@@ -3,6 +3,24 @@
  
 using namespace std;
 
+// from https://stackoverflow.com/questions/111928/is-there-a-printf-converter-to-print-in-binary-format
+// Assumes little endian
+
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+    
+    for (i = size-1; i >= 0; i--) {
+        for (j = 7; j >= 0; j--) {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
+
 struct {
     
    unsigned int bit_one : 1;
@@ -15,13 +33,28 @@ struct {
  
 int main()
 {
- 
+    struct_bitfield.bit_one = 1;
     struct_bitfield.bit_two = 0;
     struct_bitfield.bits_threefour = 1;
-    
+    cout << "bit field: ";
+    printBits(1, &struct_bitfield);
    
     bitset<8> my_bitset_decimal(19);
     bitset<8> my_bitset_string(string("1010"));
 
-    cout << my_bitset_decimal << " " << my_bitset_string;
+    cout << "bit set decimal: " << my_bitset_decimal << " bit set string: " << my_bitset_string << endl << endl;
+    
+    
+    bitset<6> all_zeroes(0);
+    
+    cout << "Bitset (all zeroes): " << all_zeroes << endl;
+    
+    all_zeroes.set(3);
+    all_zeroes.set(5);
+
+    cout << "Bitset (set bit 3 and 5): " << all_zeroes << endl;
+    cout << "Bitset (count of set bits): " << all_zeroes.count() << endl;
+    cout << all_zeroes <<  " is " << all_zeroes.to_ulong();
+
+    
 }
