9e2bb899af73d01a1319ff400549a983980f7004
benjaminkravets <bakravets@gmail.com>
2024-03-01 20:17:48 -0500

    add comments for istream/ostream demo

---
+++ STL/Iterators/istream_ostream/istream_ostream.cpp
@@ -0,0 +1,23 @@
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+ 
+using namespace std;
+
+int main()
+{
+    //iterator for user input
+    istream_iterator<int> input_iterator(cin);
+    
+    //end of user input (no space)
+    istream_iterator<int> input_end;
+
+    //output iterator to take input, delimited by spaces
+    ostream_iterator<int> output_iterator(cout, ",");
+ 
+    //copy input to output
+    copy(input_iterator, input_end, output_iterator);
+ 
+    return 0;
+}
--- scratch_space/scratch_space.cpp
+++ scratch_space/scratch_space.cpp
@@ -1,20 +1,46 @@
-
-#include <algorithm>
-#include <iostream>
-#include <iterator>
- 
-using namespace std;
-
-int main()
-{
- 
-    istream_iterator<int> input_iterator(cin);
-    
-    istream_iterator<int> eos;
- 
-    ostream_iterator<int> output_iterator(cout, " ");
- 
-    copy(input_iterator, eos, output_iterator);
- 
-    return 0;
-}
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+int main() 
+{ 
+    // Declaring a vector 
+    vector<int> v = { 1, 2, 3 }; 
+  
+    // Declaring an iterator 
+    vector<int>::iterator i; 
+  
+    int j; 
+  
+    cout << "Without iterators = "; 
+  
+    // Accessing the elements without using iterators 
+    for (j = 0; j < 3; ++j) { 
+        cout << v[j] << " "; 
+    } 
+  
+    cout << "\nWith iterators = "; 
+  
+    // Accessing the elements using iterators 
+    for (i = v.begin(); i != v.end(); ++i) { 
+        cout << *i << " "; 
+    } 
+  
+    // Adding one more element to vector 
+    v.push_back(4); 
+  
+    cout << "\nWithout iterators = "; 
+  
+    // Accessing the elements without using iterators 
+    for (j = 0; j < 4; ++j) { 
+        cout << v[j] << " "; 
+    } 
+  
+    cout << "\nWith iterators = "; 
+  
+    // Accessing the elements using iterators 
+    for (i = v.begin(); i != v.end(); ++i) { 
+        cout << *i << " "; 
+    } 
+  
+    return 0; 
+} 
