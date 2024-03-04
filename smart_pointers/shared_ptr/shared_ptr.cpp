410a805046e8fb278adaaed364125983652f4ca5
benjaminkravets <bakravets@gmail.com>
2024-03-04 16:39:17 -0500

    shared and weak pointers

---
+++ smart_pointers/shared_ptr/shared_ptr.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+ 
+
+class circle {
+    int radius;
+    string color;
+
+    public:
+    circle(int r, string c){
+        radius = r;
+        color = c;
+    }
+
+    double circumference(){
+        return 2 * 3.14159 * radius;
+    }
+
+};
+
+int main(){
+
+    //The shared is similar to a normal pointer but it still has ownership and will count how many shared pointers there are
+    shared_ptr<circle> smart_circle_ptr1(new circle(4,"teal"));
+    cout << smart_circle_ptr1->circumference() << endl;
+
+    //When a copy is made, ownership is shared, so both pointers can now be used to access the object.
+    shared_ptr<circle> smart_circle_ptr2 = smart_circle_ptr1;
+    cout << smart_circle_ptr2->circumference() << endl;
+    cout << smart_circle_ptr1->circumference() << endl;
+
+    //To confirm multiple owners the count can be accessed
+    cout << smart_circle_ptr1.use_count() << endl;
+
+}
---
+++ smart_pointers/weak_ptr/weak_ptr.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+ 
+
+class circle {
+    int radius;
+    string color;
+
+    public:
+    circle(int r, string c){
+        radius = r;
+        color = c;
+    }
+
+    double circumference(){
+        return 2 * 3.14159 * radius;
+    }
+
+};
+
+int main(){
+
+
+    //Two weak poiners can share non owning access to the same object- with no uniqueness or ownership, 
+    //what makes weak pointers special is there knowledge that the object they point to is not deallocated 
+    //danglinng memory.
+
+    shared_ptr<circle> smart_circle_ptr1(new circle(3.2, "cyan"));
+
+    //Must be created by converting from a shared ptr.
+    weak_ptr<circle> smart_circle_ptr2(smart_circle_ptr1);
+    weak_ptr<circle> smart_circle_ptr3 = smart_circle_ptr2;
+
+    //Count will still be 0 even after creating copy
+    cout << smart_circle_ptr2.use_count() << endl;
+
+
+}
