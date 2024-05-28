a92ca9c651e02f02beea8627207fefb98786f1fd
benjaminkravets <bakravets@gmail.com>
2024-05-27 22:06:49 -0400

    debug stream buffer seg fault

--- freertos/FreeRTOSv202212.01/FreeRTOS/Demo/Posix_GCC/main_stream_buffer.c
+++ freertos/FreeRTOSv202212.01/FreeRTOS/Demo/Posix_GCC/main_stream_buffer.c
@@ -19,14 +19,19 @@ static void timer_print_callback(TimerHandle_t timer_handle); // timer callback
 static TaskHandle_t task_0_print = NULL;     // print from buffer task handle
 static void task_0_main(void *pvParameters); // task main loop
 
-static StreamBufferHandle_t stream_buffer_0;
+StreamBufferHandle_t stream_buffer_0;
+static size_t stream_buffer_0_length = 4, stream_buffer_0_trigger = 4;
 
-#define BUFFER_DATA_SIZE 32
-static uint8_t buffer_data_tx[BUFFER_DATA_SIZE];
+#define BUFFER_DATA_SIZE 4
+//static uint8_t buffer_data_tx[BUFFER_DATA_SIZE];
 static uint8_t buffer_data_rx[BUFFER_DATA_SIZE];
+
+void vAFunction( StreamBufferHandle_t xStreamBuffer );
+
 
 void main_stream_buffer(void)
 {
+
 
     timer_0_print = xTimerCreate("Print Timer",         // friendly debug name
                                  PRINT_FREQUENCY_MS,    // frequency of timer callback
@@ -54,19 +59,18 @@ void main_stream_buffer(void)
         printf("Task alloc failed \r\n");
     }
 
-    stream_buffer_0 = xStreamBufferCreate(32, // size in bytes
-                                          1); // how many bytes need to be in buffer to trigger unblock
-
+    stream_buffer_0 = xStreamBufferCreate(stream_buffer_0_length,   // size in bytes
+                                          stream_buffer_0_trigger); // how many bytes need to be in buffer to trigger unblock
 
     if (stream_buffer_0 == NULL)
     {
         printf("Stream buffer alloc failed \r\n");
     }
 
-    for (uint8_t i = 0; i < BUFFER_DATA_SIZE; i++)
-    {
-        buffer_data_tx[i] = i;
-    }
+    // for (uint8_t i = 0; i < BUFFER_DATA_SIZE; i++)
+    // {
+    //     buffer_data_tx[i] = i;
+    // }
 
     vTaskStartScheduler();
 
@@ -81,31 +85,74 @@ void main_stream_buffer(void)
 
 static void timer_print_callback(TimerHandle_t timer_handle)
 {
-    printf("Sending to stream_buffer_0 \r\n");
+    // uint8_t buffer_data_tx[] = {1,2,3,4};
+    // size_t bytes_to_send = 1;
+    
 
-    size_t bytes_sent = xStreamBufferSend(stream_buffer_0,
-                                          buffer_data_tx,
-                                          8,
-                                          pdMS_TO_TICKS(0));
+    // printf("Sending to stream_buffer_0 \r\n");
+    vAFunction(stream_buffer_0);
 
-    printf("Sent %i bytes to stream_buffer_0 \r\n", bytes_sent);
+    // size_t bytes_sent = xStreamBufferSend(stream_buffer_0,
+    //                                       (void*)buffer_data_tx,
+    //                                       bytes_to_send,
+    //                                       pdMS_TO_TICKS(100));
+
+    // printf("Sent %li bytes to stream_buffer_0 \r\n", bytes_sent);
 }
 
 static void task_0_main(void *pvParameters)
 {
+    uint8_t rx_data[32];
+    uint8_t bytes_received;
 
     while (1)
     {
 
-        // uint8_t bytes_received = xStreamBufferReceive(stream_buffer_0,
-        //                                               buffer_data_rx,
-        //                                               BUFFER_DATA_SIZE,
+        // bytes_received = xStreamBufferReceive(stream_buffer_0,
+        //                                               rx_data,
+        //                                               sizeof(rx_data),
         //                                               portMAX_DELAY);
+
+        // printf("Received %i bytes \r\n", bytes_received);
 
         // for (uint8_t i = 0; i < bytes_received; i++)
         // {
-
         //     printf("%i ", buffer_data_rx[i]);
         // }
+    }
+}
+
+void vAFunction( StreamBufferHandle_t xStreamBuffer )
+{
+size_t xBytesSent;
+uint8_t ucArrayToSend[] = { 0, 1, 2, 3 };
+char *pcStringToSend = "String to send";
+const TickType_t x100ms = pdMS_TO_TICKS( 100 );
+
+    /* Send an array to the stream buffer, blocking for a maximum of 100ms to
+    wait for enough space to be available in the stream buffer. */
+    xBytesSent = xStreamBufferSend( xStreamBuffer,
+                                   ( void * ) ucArrayToSend,
+                                   sizeof( ucArrayToSend ),
+                                   x100ms );
+
+    if( xBytesSent != sizeof( ucArrayToSend ) )
+    {
+        /* The call to xStreamBufferSend() times out before there was enough
+        space in the buffer for the data to be written, but it did
+        successfully write xBytesSent bytes. */
+    }
+
+    /* Send the string to the stream buffer.  Return immediately if there is not
+    enough space in the buffer. */
+    xBytesSent = xStreamBufferSend( xStreamBuffer,
+                                    ( void * ) pcStringToSend,
+                                    strlen( pcStringToSend ), 0 );
+
+    if( xBytesSent != strlen( pcStringToSend ) )
+    {
+        /* The entire string could not be added to the stream buffer because
+        there was not enough free space in the buffer, but xBytesSent bytes
+        were sent.  Could try again to send the remaining bytes. */
     }
 }
--- scratch_space/scratch_space.c
+++ scratch_space/scratch_space.c
@@ -10,6 +10,11 @@ int main () {
    //what does this do
    char *text = calloc(1,1), buffer[32];
 
+   unsigned char input[20];
+
+   fgets(input, sizeof(input), stdin);
+
+   printf("%s \r\n", input);
 
    return 0;
 }
