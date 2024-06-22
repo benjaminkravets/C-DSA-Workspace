7dc89da6cf7845cb35435f91c3d745feebdbfbd2
benjaminkravets <bakravets@gmail.com>
2024-06-22 16:25:42 -0400

    add event bit demo

--- freertos/FreeRTOSv202212.01/FreeRTOS/Demo/Posix_GCC/main_event_bits.c
+++ freertos/FreeRTOSv202212.01/FreeRTOS/Demo/Posix_GCC/main_event_bits.c
@@ -6,6 +6,7 @@
 #include "task.h"
 #include "timers.h"
 #include "semphr.h"
+#include "event_groups.h"
 
 /* Local includes. */
 #include "console.h"
@@ -14,6 +15,11 @@
 
 static TimerHandle_t timer_0_print = NULL;                    // timer handle
 static void timer_print_callback(TimerHandle_t timer_handle); // timer callback
+
+static TaskHandle_t task_0_print = NULL;     // print task handle
+static void task_0_main(void *pvParameters); // task main loop
+
+static EventGroupHandle_t event_group_0; //event group handle (has 32 bits via configUSE_16_BIT_TICKS)
 
 void main_event_bits(void)
 {
@@ -33,6 +39,24 @@ void main_event_bits(void)
         printf("Timer alloc failed \r\n");
     }
 
+    BaseType_t task_created = xTaskCreate(task_0_main,              // entry function
+                                          "Print task",             // debug name
+                                          configMINIMAL_STACK_SIZE, // stack size (words)
+                                          NULL,                     // parameters to entry function
+                                          tskIDLE_PRIORITY + 1,     // priority
+                                          &task_0_print);           // task handle
+    if (task_created != pdPASS)
+    {
+        printf("Task alloc failed \r\n");
+    }
+
+    event_group_0 = xEventGroupCreate();
+
+    if (event_group_0 == NULL)
+    {
+        printf("Event group alloc failed");
+    }
+
     vTaskStartScheduler();
 
     /* If all is well, the scheduler will now be running, and the following
@@ -46,5 +70,19 @@ void main_event_bits(void)
 
 static void timer_print_callback(TimerHandle_t timer_handle)
 {
-    printf("Timer called \r\n");
+
+    xEventGroupSetBits(event_group_0, (1 << 5));
+    printf("TImer 0 set event bit 5 \r\n");
+}
+
+static void task_0_main(void *pvParameters)
+{
+
+    while (1)
+    {
+
+        EventBits_t set_bits = xEventGroupWaitBits(event_group_0, (1 << 5), pdTRUE, pdFALSE, portMAX_DELAY);
+
+        printf("Task 0 unblocked on bit 5 set\r\n");
+    }
 }
--- string_storage/string_storage.c
+++ string_storage/string_storage.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char string_1[14];
-char string_2[14] = "Hello World 2"; // stored in .data
+char string_1[14]; // uninitialized global stored in .bss
+char string_2[14] = "Hello World 2"; // initialized global stored in .data
+const char string_6[14] = "Hellow World 6"; // const stored in .rodata
+
+
 
 int main()
 {
-    char string_3[14] = "Hello World 3";
 
-    strcpy(string_1, "Hello World 1"); // "Hello World 1" is stored in .text since it is an argument
+    strcpy(string_1, "Hello World 1"); // "Hello World 1" (the argument to strcpy) is stored in .text since it is an argument
+    
+    char string_3[14] = "Hello World 3"; // local stored in stack
 
-    printf("%s %s %s \r\n", string_1, string_2, string_3); // "%s %s %s" is stored in .rodata
+    char * string_4 = (char *)malloc(14); // dynamically allocated stored in heap (pointer itself is on stack)
+
+    strcpy(string_4, "Hello World 4"); // "Hello World 4" (the argument to strcpy) is stored in .text since it is an argument
+
+    static char string_5[14] = "Hello World 5"; // local static stored in .data
+
+    printf("\r\n");
+
+    return 0;
 
 }
