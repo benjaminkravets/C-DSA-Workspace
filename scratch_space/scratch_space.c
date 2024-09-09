00686d737575bbc8cffd153eba63da72874263b1
benjaminkravets <70412290+benjaminkravets@users.noreply.github.com>
2024-09-09 12:34:43 -0400

    add post send int tokenization

--- scratch_space/scratch_space.c
+++ scratch_space/scratch_space.c
@@ -3,10 +3,11 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <string.h>
+#include <ctype.h>
 
-
-void paws(){
-    uint8_t foo[] = "S PBCDE P 1000 S EDFSJ P 0x10 S sde END";
+void paws()
+{
+    uint8_t foo[] = "S 1 56 78 255 3 4 P 10000 S 45 6 8 9 62 2 P 2000 SR 1 32 54 234 E";
 
     uint8_t *next_tok;
 
@@ -14,33 +15,45 @@ void paws(){
 
     do
     {
-        if (next_tok != NULL)
 
-            if (strcmp(next_tok, "S") == 0)
+        if (strcmp(next_tok, "S") == 0)
+        {
+            printf("%s send ", next_tok);
+            do
             {
-                printf("%s send %s\n", next_tok, strtok(NULL, " "));
-            }
-            else if (strcmp(next_tok, "R") == 0)
-            {
-                printf("%s receive %s\n", next_tok, strtok(NULL, " "));
-            }
-            else if (strcmp(next_tok, "P") == 0)
-            {
-                printf("%s pause %s\n", next_tok, strtok(NULL, " "));
-            }
-            else if (strcmp(next_tok, "END") == 0)
-            {
-                printf("%s end\n", next_tok);
-            }
+                printf("%s ", next_tok);
+                next_tok = strtok(NULL, " ");
+
+            } while (isdigit(*next_tok));
+
+            printf("\n");
+
+            continue;
+        }
+        else if (strcmp(next_tok, "R") == 0)
+        {
+            printf("%s receive %s\n", next_tok, strtok(NULL, " "));
+        }
+        else if (strcmp(next_tok, "P") == 0)
+        {
+            printf("%s pause %s\n", next_tok, strtok(NULL, " "));
+        }
+        else if (strcmp(next_tok, "END") == 0)
+        {
+            printf("%s end\n", next_tok);
+        }
 
         next_tok = strtok(NULL, " ");
 
     } while (next_tok != NULL);
+}
 
+void test(){
+    printf("%i \n", isdigit('9'));
 }
 
 int main()
 {
-
+    //test();
     paws();
 }
