24ca7c7f5d844ce3cac7f5979b3ff21656215386
benjaminkravets <bakravets@gmail.com>
2025-02-24 21:29:56 -0500

    add enable_if examples

--- scratch_space/scratch_space.cpp
+++ scratch_space/scratch_space.cpp
@@ -4,25 +4,71 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
+#include <type_traits>
+#include <iostream>
 using namespace std;
 
+// enable_if intentionally causes substitution failures (don't consider this for overload resolution)
 
-template <class T,
-         typename std::enable_if<std::is_integral<T>::value,
-                                 T>::type* = nullptr>
-void do_stuff(T& t) {
-  std::cout << "do_stuff integral\n";
-    // an implementation for integral types (int, char, unsigned, etc.)
+template <bool Condition, typename T>
+// if Condition is true, the typedef 'type' is defined to be second parameter (void by default)
+typename enable_if<Condition, void>::type // this line says 'if Condition is True, use type void'- effectively this line could be 'void'
+foo(T value)
+{
+    std::cout << "True case: " << value << std::endl;
 }
 
-template <class T,
-          typename std::enable_if<std::is_class<T>::value,
-                                  T>::type* = nullptr>
-void do_stuff(T& t) {
-    // an implementation for class types
+template <bool Condition, typename T>
+typename enable_if<!Condition, void>::type
+foo(T value)
+{
+    std::cout << "False case: " << value << std::endl;
 }
 
-int main() {
+// type trait evaluations can be used as conditions in enable_if
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, void>::type bar(T x)
+{
+    cout << "integer" << endl;
+}
 
+template <typename T>
+typename std::enable_if<std::is_floating_point<T>::value, void>::type bar(T x)
+{
+    cout << "float" << endl;
+}
+
+template <typename T>
+typename std::enable_if<!std::is_integral<T>::value && !std::is_floating_point<T>::value, void>::type bar(T x)
+{
+    cout << "other" << endl;
+}
+
+// enable_if can also be used to disqualify function templates based on paramter types
+// here, the second function parameter isn't used by the function body, it helps determine which template should be used
+template <typename T>
+void fonzie(T a, typename enable_if<is_integral<T>::value, int>::type = 0)
+{
+    cout << "integer" << endl;
+}
+
+template <typename T>
+void fonzie(T a, typename enable_if<is_floating_point<T>::value, int>::type = 0)
+{
+    cout << "float" << endl;
+}
+
+int main()
+{
+    // foo<true>(42);
+    // foo<false>("Hello");
+
+    // bar(1);
+    // bar(1.2);
+    // bar("Hello world");
+
+    fonzie(1);
+    fonzie(1.2);
+
+    return 0;
 }
